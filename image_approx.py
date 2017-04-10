a4a3f251fca30d673e8c64d332500cb928ee1bae
joeylmaalouf <joeylmaalouf@gmail.com>
2017-04-10 14:44:50 -0400

    add triangles instead of modify ovals

--- image_approx.py
+++ image_approx.py
@@ -4,89 +4,113 @@ from random import randint
 import sys
 
 
-class Ellipse (object):
-  def __init__ (self, resolution):
+class Triangle (object):
+  def __init__ (self, resolution, attributes = {}):
     self.resolution = resolution
-    for i in range(5):
-      self.randomize(i)
+    self.attributes = {
+      "x1": 0, "y1": 0,
+      "x2": 0, "y2": 0,
+      "x3": 0, "y3": 0,
+      "r":  0, "g":  0, "b":  0, "a":  0
+    }
+    for attrib in self.attributes.keys():
+      if attrib in attributes.keys():
+        self.attributes[attrib] = attributes[attrib]
+      else:
+        self.randomize(attrib)
 
-  def randomize (self, attribute_index):
-    if attribute_index == 0:
-      self.xcoord = randint(0, self.resolution[0] - 1)
-    elif attribute_index == 1:
-      self.ycoord = randint(0, self.resolution[1] - 1)
-    elif attribute_index == 2:
-      self.xradius = randint(1, self.resolution[0] - 1)
-    elif attribute_index == 3:
-      self.yradius = randint(1, self.resolution[1] - 1)
-    elif attribute_index == 4:
-      self.brightness = randint(0, 255)
+  def randomize (self, attribute):
+    if   attribute == "x1":
+      self.attributes["x1"] = randint(0, self.resolution[0] - 1)
+    elif attribute == "y1":
+      self.attributes["y1"] = randint(0, self.resolution[1] - 1)
+    elif attribute == "x2":
+      self.attributes["x2"] = randint(0, self.resolution[0] - 1)
+    elif attribute == "y2":
+      self.attributes["y2"] = randint(0, self.resolution[1] - 1)
+    elif attribute == "x3":
+      self.attributes["x3"] = randint(0, self.resolution[0] - 1)
+    elif attribute == "y3":
+      self.attributes["y3"] = randint(0, self.resolution[1] - 1)
+    elif attribute == "r":
+      self.attributes["r"] = randint(0, 255)
+    elif attribute == "g":
+      self.attributes["g"] = randint(0, 255)
+    elif attribute == "b":
+      self.attributes["b"] = randint(0, 255)
+    elif attribute == "a":
+      self.attributes["a"] = randint(0, 255)
 
 
 class Individual (object):
   def __init__ (self, resolution):
-    self.n_ellipses = 20
-    self.solution = [Ellipse(resolution) for _ in range(self.n_ellipses)]
+    self.resolution = resolution
+    self.solution = []
     self.fitness = 0
 
-  def make_image (self, resolution):
-    self.image = Image.new("L", resolution)
+  def make_image (self):
+    self.image = Image.new("RGB", self.resolution)
     draw = ImageDraw.Draw(self.image)
-    for i in range(self.n_ellipses):
-      ellipse = self.solution[i]
-      bounds = (ellipse.xcoord - ellipse.xradius, ellipse.ycoord - ellipse.yradius,
-                ellipse.xcoord + ellipse.xradius, ellipse.ycoord + ellipse.yradius)
-      draw.ellipse(bounds, fill = ellipse.brightness)
+    for triangle in self.solution:
+        draw.polygon([(triangle.attributes["x1"], triangle.attributes["y1"]),
+                      (triangle.attributes["x2"], triangle.attributes["y2"]),
+                      (triangle.attributes["x3"], triangle.attributes["y3"])],
+                     fill = (triangle.attributes["r"],
+                             triangle.attributes["g"],
+                             triangle.attributes["b"],
+                             triangle.attributes["a"]))
 
-  def mutate (self):
-    i = randint(0, self.n_ellipses - 1)
-    j = randint(0, 4)
-    self.solution[i].randomize(j)
+  def evolve (self):
+      self.solution.append(Triangle(self.resolution))
 
-  def scramble (self, n = 100):
-    for _ in range(n):
-      self.mutate()
-
-  def payoff (self, resolution, goal_access):
-    diff = 0
+  def payoff (self, goal_access):
     self_access = self.image.load()
-    for i in range(resolution[0]):
-      for j in range(resolution[1]):
-        diff += abs(self_access[i, j] - goal_access[i, j])
+    diff = 0
+    for i in range(self.resolution[0]):
+      for j in range(self.resolution[1]):
+        self_pix = self_access[i, j]
+        goal_pix = goal_access[i, j]
+        diff += abs(self_pix[0] - goal_pix[0])
+        diff += abs(self_pix[1] - goal_pix[1])
+        diff += abs(self_pix[2] - goal_pix[2])
     self.fitness = diff ** 2
+
+  def copy (self):
+    copy = Individual(self.resolution)
+    copy.solution = [Triangle(self.resolution, triangle.attributes) for triangle in self.solution]
+    return copy
 
 
 def main (goal_path, popsize, generations, step):
   folder_path = goal_path + " images"
   if not os.path.isdir(folder_path):
     os.mkdir(folder_path)
-  goal_image = Image.open(goal_path).convert("L")
+  goal_image = Image.open(goal_path).convert("RGB")
   goal_access = goal_image.load()
   resolution = goal_image.size
   population = [Individual(resolution) for i in range(popsize)]
   generation = 0
   while(generation < generations):
     for individual in population:
-      individual.make_image(resolution)
-      individual.payoff(resolution, goal_access)
+      individual.make_image()
+      individual.payoff(goal_access)
     population.sort(key = lambda x: x.fitness)
-    if generation % 100 == 0:
+    if generation % step == 0:
       population[0].image.save(
         "{0}/gen {1}, off by {2}.jpg".format(
           folder_path, generation, population[0].fitness
         )
       )
-    for individual in population[popsize // 4:popsize // 2]:
-      individual.mutate()
-    for individual in population[popsize // 2:]:
-      individual.scramble()
+    for i in range(len(population)):
+      if i > 0:
+        population[i] = population[0].copy()
+      population[i].evolve()
     generation += 1
-    print(generation)
 
 
 if __name__ == "__main__":
-  goal_path = "goal.jpg" if len(sys.argv) < 2 else sys.argv[1]
-  popsize = 1000 if len(sys.argv) < 3 else int(sys.argv[2])
-  generations = 2000 if len(sys.argv) < 4 else int(sys.argv[3])
-  step = 100 if len(sys.argv) < 5 else int(sys.argv[4])
+  goal_path   = "goal.jpg" if len(sys.argv) < 2 else sys.argv[1]
+  popsize     = 1000       if len(sys.argv) < 3 else int(sys.argv[2])
+  generations = 2001       if len(sys.argv) < 4 else int(sys.argv[3])
+  step        = 100        if len(sys.argv) < 5 else int(sys.argv[4])
   main(goal_path, popsize, generations, step)
